38d07ba31989daadc45d06ee3a96cf4652faa080
taka_rl <takaya.murakami.uni@gmail.com>
2025-11-27 10:54:57 +0900

    updated the comment

--- src/utilities/MathUtils.h
+++ src/utilities/MathUtils.h
@@ -2,8 +2,6 @@
 
 inline constexpr float PI = 3.14159265358979323846f;
 
-// angle helpers
-// PI is defined in BicycleModel.h
+// Angle helpers using PI above, wrap to (-PI, PI].
 inline float wrapPi(float a){ while(a<=-PI)a+=2*PI; while(a>PI)a-=2*PI; return a; }
 inline float lerpAngle(float a,float b,float t){ float d=wrapPi(b-a); return wrapPi(a + d*t); }
-
