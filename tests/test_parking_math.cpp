fa7ae3f694d183097e2156d99a6768bc5d03d8d3
taka_rl <takaya.murakami.uni@gmail.com>
2026-01-07 21:58:25 +0900

    modified header include path

--- tests/test_parking_math.cpp
+++ tests/test_parking_math.cpp
@@ -2,11 +2,11 @@
 #include <array>
 #include <cmath>
 
-#include "../envs/ParkingEnv.h"
-#include "../envs/ParkingParams.h"
-#include "../vehicledynamics/VehicleTypes.h"
-#include "../utilities/Randomizer.h"
-#include "../utilities/MathUtils.h"
+#include "src/envs/ParkingEnv.h"
+#include "src/envs/ParkingParams.h"
+#include "src/vehicledynamics/VehicleTypes.h"
+#include "src/utilities/Randomizer.h"
+#include "src/utilities/MathUtils.h"
 
 
 // temporal function for temporary
